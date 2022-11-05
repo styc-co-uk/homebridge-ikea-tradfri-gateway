9dad20621671827f001db378c9694f0a91c78ad9
Schoen Cho <13725288+Fourthbus@users.noreply.github.com>
2022-11-05 15:29:37 +0000

    Update air-purifier.js
    add sensor state
    
    Revert "Update air-purifier.js"
    
    This reverts commit f9b1bd84606345e33ab503eb0690c450b988eb51.
    
    debug stuck at 'updating status...' issue

--- src/air-purifier.js
+++ src/air-purifier.js
@@ -14,9 +14,9 @@ module.exports = class AirPurifier extends Device {
         this.addService('airQualitySensor', this.airQualitySensor);
         this.enablePower();
         this.enableSpeed();
-        this.enableState();
         this.enableAirQuality();
         this.enableControlLock();
+        this.enableState();
         this.enableStatus();
     }
 
@@ -24,9 +24,9 @@ module.exports = class AirPurifier extends Device {
         super.deviceChanged();
         this.updatePower();
         this.updateSpeed();
-        this.updateState();
         this.updateAirQuality();
         this.updateControlLock();
+        this.updateState();
         this.updateStatus();
     }
 
@@ -47,11 +47,11 @@ module.exports = class AirPurifier extends Device {
         autoSpeed.on('get', (callback) => {
             callback(null, this.autoSpeed);
         });
-        percentSpeed.on('get', (callback) => {
-            callback(null, this.percentSpeed);
-        });
         autoSpeed.on('set', (value, callback) => {
             this.setSpeed(value, this.percentSpeed, callback);
+        });
+        percentSpeed.on('get', (callback) => {
+            callback(null, this.percentSpeed);
         });
         percentSpeed.on('set', (value, callback) => {
             this.setSpeed(this.autoSpeed, value, callback);
@@ -68,8 +68,8 @@ module.exports = class AirPurifier extends Device {
     }
 
     enableAirQuality() {
-        var airQuality = this.airPurifier.getCharacteristic(this.Characteristic.AirQuality);
-        var pm2_5Density = this.airPurifier.getCharacteristic(this.Characteristic.PM2_5Density);
+        var airQuality = this.airQualitySensor.getCharacteristic(this.Characteristic.AirQuality);
+        var pm2_5Density = this.airQualitySensor.getCharacteristic(this.Characteristic.PM2_5Density);
         airQuality.on('get', (callback) => {
             callback(null, this.airQuality);
         });
@@ -92,20 +92,24 @@ module.exports = class AirPurifier extends Device {
 
     enableStatus() {
         var alive = this.airPurifier.addCharacteristic(this.Characteristic.StatusActive);
+        var aliveSensor = this.airQualitySensor.addCharacteristic(this.Characteristic.StatusActive);
 
         alive.on('get', (callback) => {
             this.log('Purifier %s in currently %s.', this.name, this.device.alive ? 'ALIVE' : 'DEAD');
             callback(null, this.device.alive);
         });
-
+        aliveSensor.on('get', (callback) => {
+            this.log.debug('Sensors %s in currently %s.', this.name, this.active ? 'ALIVE' : 'DEAD');
+            callback(null, this.active);
+        });
         this.updateStatus();
     }
 
     setPower(value, callback) {
-        this.log('Setting active to %s on air purifier \'%s\'', value ? 'ACTIVE' : 'INACTIVE', this.name);
+        this.log('Setting active to %s on air purifier \'%s\'.', value ? 'ACTIVE' : 'INACTIVE', this.name);
         if (this.active == value) {
         } else {
-            this.active = value
+            this.active = value;
             this.platform.gateway.operateAirPurifier(this.device, {
                 fanMode: this.active
             })
@@ -114,14 +118,14 @@ module.exports = class AirPurifier extends Device {
                     callback();
             })
             .catch((error) => {
-                this.log(error);
+                this.log.debug(error);
             });
         };
     }
 
     setSpeed(autoSpeed, percentSpeed, callback) {
-        this.log('Setting control mode to %s on air purifier \'%s\'', autoSpeed ? 'AUTO' : 'MANUAL', this.name);
-        this.log('Setting speed to %s%% on air purifier \'%s\'', percentSpeed, this.name);
+        this.log('Setting control mode to %s on air purifier \'%s\'.', autoSpeed ? 'AUTO' : 'MANUAL', this.name);
+        this.log('Setting speed to %s%% on air purifier \'%s\'.', percentSpeed, this.name);
         this.fanSpeed = percentSpeed/2 | 0;
         if (autoSpeed == 0) {
             this.fanMode = this.fanSpeed;
@@ -129,7 +133,7 @@ module.exports = class AirPurifier extends Device {
             this.fanMode = 1;
             this.autoSpeed = 1;
         };
-        this.log('SET - FANMODE', this.fanMode, 'FANSPEED (0-50)', this.fanSpeed);
+        this.log.debug('SET - FANMODE', this.fanMode, 'FANSPEED (0-50)', this.fanSpeed);
         this.platform.gateway.operateAirPurifier(this.device, {
             fanMode: this.fanMode,
             fanSpeed: this.fanSpeed
@@ -143,7 +147,7 @@ module.exports = class AirPurifier extends Device {
     }
 
     setControlLock(value, callback) {
-        this.log('Setting physical controls to %s on air purifier \'%s\'', value ? 'LOCK' : 'UNLOCK', this.name);
+        this.log('Setting child lock to %s on air purifier \'%s\'.', value ? 'LOCK' : 'UNLOCK', this.name);
         this.lockPhysicalControls = value;
 
         this.platform.gateway.operateAirPurifier(this.device, {
@@ -161,8 +165,12 @@ module.exports = class AirPurifier extends Device {
     updatePower() {
         var purifier = this.device.airPurifierList[0];
         var active = this.airPurifier.getCharacteristic(this.Characteristic.Active);
+        if (active.value != this.active) {
+            this.log('Updating active to \'%s\' on air purifier \'%s\'.', this.active ? 'ACTIVE' : 'INACTIVE', this.name);
+        } else {
+            this.log.debug('Updating active to \'%s\' on air purifier \'%s\'.', this.active ? 'ACTIVE' : 'INACTIVE', this.name);
+        }
         this.active = (purifier.fanMode != 0);
-        this.log('Updating active to %s on air purifier \'%s\'', this.active ? 'ACTIVE' : 'INACTIVE', this.name);
         active.updateValue(this.active);
     }
 
@@ -170,34 +178,57 @@ module.exports = class AirPurifier extends Device {
         var purifier = this.device.airPurifierList[0];
         var autoSpeed = this.airPurifier.getCharacteristic(this.Characteristic.TargetAirPurifierState);
         var percentSpeed = this.airPurifier.getCharacteristic(this.Characteristic.RotationSpeed);
-        this.percentSpeed = purifier.fanSpeed*2;
-        this.log('Updating speed to %s%% on air purifier \'%s\'', this.percentSpeed, this.name);
-        percentSpeed.updateValue(this.percentSpeed);
-        if (purifier.fanMode > 1) {
-            this.autoSpeed = 0;
+        if (this.percentSpeed != purifier.fanSpeed*2) {
+            this.log('Updating speed to %s%% on air purifier \'%s\'.', this.percentSpeed, this.name);
         } else {
+            this.log.debug('Updating speed to %s%% on air purifier \'%s\'.', this.percentSpeed, this.name);
+        }
+
+        this.percentSpeed = purifier.fanSpeed*2;
+        percentSpeed.updateValue(this.percentSpeed);
+
+        if (this.autoSpeed != autoSpeed.value) {
+            this.log('Updating control mode to \'%s\' on air purifier \'%s\'.', this.autoSpeed ? 'AUTO' : 'MANUAL', this.name);
+        } else {
+            this.log.debug('Updating control mode to \'%s\' on air purifier \'%s\'.', this.autoSpeed ? 'AUTO' : 'MANUAL', this.name);
+        }
+        if (purifier.fanMode == 1 || purifier.fanMode == 0) {
             this.autoSpeed = 1;
+        } else {
+            this.autoSpeed = 0;
         };
-        this.log('Updating control mode to %s on air purifier \'%s\'', this.autoSpeed ? 'AUTO' : 'MANUAL', this.name);
         autoSpeed.updateValue(this.autoSpeed);
     }
 
     updateState() {
         var purifier = this.device.airPurifierList[0];
         var currentAirPurifierState = this.airPurifier.getCharacteristic(this.Characteristic.CurrentAirPurifierState);
-        if (purifier.fanSpeed >= 1) {
+        if (currentAirPurifierState.value != this.currentAirPurifierState) {
+            this.log('Updating state to %s on air purifier \'%s\'.', this.currentAirPurifierState/2 ? 'PURIFYING_AIR' : 'INACTIVE', this.name);
+        } else {
+            this.log.debug('Updating state to %s on air purifier \'%s\'.', this.currentAirPurifierState/2 ? 'PURIFYING_AIR' : 'INACTIVE', this.name);
+        }
+
+        if (purifier.fanMode == 0 || purifier.fanSpeed == 0) {
+            this.currentAirPurifierState = 0;
+        } else if (purifier.fanSpeed > 0) {
             this.currentAirPurifierState = 2;
         } else {
             this.currentAirPurifierState = 0;
         }
-        this.log('Updating state to %s on air purifier \'%s\'', this.currentAirPurifierState/2 ? 'PURIFYING_AIR' : 'INACTIVE', this.name);
         currentAirPurifierState.updateValue(this.currentAirPurifierState);
     }
 
     updateAirQuality() {
         var purifier = this.device.airPurifierList[0];
-        var airQuality = this.airPurifier.getCharacteristic(this.Characteristic.AirQuality);
-        var pm2_5Density = this.airPurifier.getCharacteristic(this.Characteristic.PM2_5Density);
+        var airQuality = this.airQualitySensor.getCharacteristic(this.Characteristic.AirQuality);
+        var pm2_5Density = this.airQualitySensor.getCharacteristic(this.Characteristic.PM2_5Density);
+        if (this.pm2_5Density != purifier.airQuality) {
+            this.log('Updating air quality to PM2.5 \= %sppm \(%s\) on air purifier \'%s\'.', this.pm2_5Density, this.airQualityDesc, this.name);
+        } else {
+            this.log.debug('Updating air quality to PM2.5 \= %sppm \(%s\) on air purifier \'%s\'.', this.pm2_5Density, this.airQualityDesc, this.name);
+        }
+
         this.pm2_5Density = purifier.airQuality;
         var airQualityDesc = 'UNKNOWN';
         if (purifier.airQuality > 0 && purifier.airQuality <= 15) {
@@ -219,33 +250,42 @@ module.exports = class AirPurifier extends Device {
             this.airQuality = 0;
             this.pm2_5Density = 0;
         };
-        this.log('Updating PM2.5 density to %s ppm on air purifier \'%s\'', this.pm2_5Density, this.name);
-        this.log('Updating air quality to %s on air purifier \'%s\'', this.airQualityDesc, this.name);
         airQuality.updateValue(this.airQuality);
         pm2_5Density.updateValue(this.pm2_5Density);
-        this.updateAirQualitySensor();
+        // this.updateAirQualitySensor();
     }
 
-    updateAirQualitySensor() {
-        var airQuality = this.airQualitySensor.getCharacteristic(this.Characteristic.AirQuality);
-        var pm2_5Density = this.airQualitySensor.getCharacteristic(this.Characteristic.PM2_5Density);
-        airQuality.updateValue(this.airQuality);
-        pm2_5Density.updateValue(this.pm2_5Density);
-    }
+    // updateAirQualitySensor() {
+    //     var airQuality = this.airQualitySensor.getCharacteristic(this.Characteristic.AirQuality);
+    //     var pm2_5Density = this.airQualitySensor.getCharacteristic(this.Characteristic.PM2_5Density);
+    //     airQuality.updateValue(this.airQuality);
+    //     pm2_5Density.updateValue(this.pm2_5Density);
+    // }
 
     updateControlLock() {
         var purifier = this.device.airPurifierList[0];
         var lockPhysicalControls = this.airPurifier.getCharacteristic(this.Characteristic.LockPhysicalControls);
+        if (this.lockPhysicalControls != purifier.controlsLocked) {
+            this.log('Updating child lock to %s on air purifier \'%s\'.', this.lockPhysicalControls ? 'LOCK' : 'UNLOCK', this.name);
+        } else {
+            this.log.debug('Updating child lock to %s on air purifier \'%s\'.', this.lockPhysicalControls ? 'LOCK' : 'UNLOCK', this.name);
+        }
+
         this.lockPhysicalControls = purifier.controlsLocked;
-        this.log('Updating physical controls to %s on air purifier \'%s\'', this.lockPhysicalControls ? 'LOCK' : 'UNLOCK', this.name);
         lockPhysicalControls.updateValue(this.lockPhysicalControls);
     }
         
     updateStatus() {
         var alive = this.airPurifier.getCharacteristic(this.Characteristic.StatusActive);
+        var aliveSensor = this.airQualitySensor.getCharacteristic(this.Characteristic.StatusActive);
+        if (alive.value != this.device.alive) {
+            this.log('Updating active status to %s on air purifier \'%s\'.', this.device.alive ? 'ALIVE' : 'DEAD', this.name);
+        } else {
+            this.log.debug('Updating active status to %s on air purifier \'%s\'.', this.device.alive ? 'ALIVE' : 'DEAD', this.name);
+        }
 
-        this.log('Updating active status to %s on air purifier \'%s\'', this.device.alive ? 'ALIVE' : 'DEAD', this.name);
         alive.updateValue(this.device.alive);
+        aliveSensor.updateValue(this.active);
     }
 
 };
